57bc89a2b4d73f3132e8b280a2b84dbb9aa16f5d
Xinyi Zhang <luciaxinyi.zhang@mail.utoronto.ca>
2020-12-14 02:18:27 -0500

    Still working on it

---
+++ R/Data.R
@@ -0,0 +1,12 @@
+# Sequence of genes
+# @source \url{https://www.genecards.org/cgi-bin/carddisp.pl?gene=CYP17A1}
+# @source \url{https://www.genecards.org/cgi-bin/carddisp.pl?gene=FCGBP&keywords=FCGBP}
+# @source \url{https://www.genecards.org/cgi-bin/carddisp.pl?gene=GGT5&keywords=GGT5}
+# @source \url{https://www.genecards.org/cgi-bin/carddisp.pl?gene=FAM230B&keywords=FAM230B}
+# @source \url{https://www.genecards.org/cgi-bin/carddisp.pl?gene=GOLGA8J&keywords=GOLGA8J}
+# @source \url{https://www.genecards.org/cgi-bin/carddisp.pl?gene=APBB1IP&keywords=APBB1IP}
+# @source \url{https://www.genecards.org/cgi-bin/carddisp.pl?gene=DSCAM&keywords=DSCAM}
+# @source \url{https://www.genecards.org/cgi-bin/carddisp.pl?gene=RASA4B&keywords=RASA4B}
+# @source \url{https://www.genecards.org/cgi-bin/carddisp.pl?gene=PATL1&keywords=PATL1}
+# @source \url{https://www.genecards.org/cgi-bin/carddisp.pl?gene=PPP2R3B&keywords=PPP2R3B}
+# @source \url{https://www.genecards.org/cgi-bin/carddisp.pl?gene=DIAPH2&keywords=DIAPH2}
