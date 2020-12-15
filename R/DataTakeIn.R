dfb3a94faea0bb6c7e0c3eaa59b3ccc5987f703f
Xinyi Zhang <luciaxinyi.zhang@mail.utoronto.ca>
2020-12-14 20:01:25 -0500

    still working

--- R/DataTakeIn.R
+++ R/DataTakeIn.R
@@ -8,8 +8,8 @@ inPutNucleotide <- function()
 inPutTrait <- function()
   my.trait1 <- readline(prompt = "Enter the first trait associated")
   print(paste("First trait", my.trait1, "saved."))
-  my.trait1 <- readline(prompt = "Enter the second trait associated")
-  print(paste("Second trait", my.trait1, "saved."))
+  my.trait2 <- readline(prompt = "Enter the second trait associated")
+  print(paste("Second trait", my.trait2, "saved."))
 
 
 
