c1b1f966443a6cc80835c00f73a72f835c30a0c3
Xinyi Zhang <luciaxinyi.zhang@mail.utoronto.ca>
2020-12-15 01:39:45 -0500

    working on data structure

--- R/DataTakeIn.R
+++ R/DataTakeIn.R
@@ -1,20 +1,26 @@
 #Functions let client to input nucleotide and associated traits wanted to be studied.
 #
 
-#Let client to input gene numeber and nucleotide number.
-inPutNucleotide <- function()
+#Let client to input gene number and nucleotide number.
+inputNucleotide <- function()
   my.gene <- readline(prompt = "Enter the gene to be studied")
   print(paste("Nucleotide", my.name, "saved."))
   my.nucleotide <- readline(prompt = "Enter the nucleotide to be studied")
   print(paste("Nucleotide", my.name, "saved."))
 
 #Let client to input traits to be studied.
-  inPutTrait <- function()
+inputTrait <- function()
   my.trait1 <- readline(prompt = "Enter the first trait associated")
   print(paste("First trait", my.trait1, "saved."))
   my.trait2 <- readline(prompt = "Enter the second trait associated")
   print(paste("Second trait", my.trait2, "saved."))
-
+  trait_list <- list(my.trait1, my.trait2)
+  my.bool <- readline(prompt = "Do you need to enter more traits?")
+  while (my.bool = "Yes"){
+    my.trait <- readline(prompt = "Enter further trait associated")
+    list.append(trait_list, my.trait)
+  }
+  return(trait_list)
 
 
 
--- R/PhenoStats.R
+++ R/PhenoStats.R
@@ -1,4 +1,6 @@
 # This is a function to make statistics for traits shown in different genotypes
 #
 
- <- function(trait1, trait2, nucleodata) {
+myFunction  <- function()
+  trait1 <- my.trait1
+  trait2 <- my.trait2
