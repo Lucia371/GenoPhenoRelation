c8fbfdf8f68bb21ef695e331b5912540f94c66e7
Xinyi Zhang <luciaxinyi.zhang@mail.utoronto.ca>
2020-12-14 20:06:03 -0500

    Still working

--- R/DataTakeIn.R
+++ R/DataTakeIn.R
@@ -1,15 +1,21 @@
-#A function let client to input nucleotide data and trait
+#Functions let client to input nucleotide and associated traits wanted to be studied.
 #
 
+#Let client to input gene numeber and nucleotide number.
 inPutNucleotide <- function()
+  my.gene <- readline(prompt = "Enter the gene to be studied")
+  print(paste("Nucleotide", my.name, "saved."))
   my.nucleotide <- readline(prompt = "Enter the nucleotide to be studied")
   print(paste("Nucleotide", my.name, "saved."))
 
-inPutTrait <- function()
+#Let client to input traits to be studied.
+  inPutTrait <- function()
   my.trait1 <- readline(prompt = "Enter the first trait associated")
   print(paste("First trait", my.trait1, "saved."))
   my.trait2 <- readline(prompt = "Enter the second trait associated")
   print(paste("Second trait", my.trait2, "saved."))
+
+
 
 
 
