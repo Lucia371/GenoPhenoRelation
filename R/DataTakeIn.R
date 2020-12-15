4964c6b08bafb7e01bb70bb22698779a13bc0f4a
Xinyi Zhang <luciaxinyi.zhang@mail.utoronto.ca>
2020-12-14 20:00:24 -0500

    Still working

--- R/DataTakeIn.R
+++ R/DataTakeIn.R
@@ -1,7 +1,16 @@
 #A function let client to input nucleotide data and trait
 #
 
-inPutTrait <- function()
-
-
 inPutNucleotide <- function()
+  my.nucleotide <- readline(prompt = "Enter the nucleotide to be studied")
+  print(paste("Nucleotide", my.name, "saved."))
+
+inPutTrait <- function()
+  my.trait1 <- readline(prompt = "Enter the first trait associated")
+  print(paste("First trait", my.trait1, "saved."))
+  my.trait1 <- readline(prompt = "Enter the second trait associated")
+  print(paste("Second trait", my.trait1, "saved."))
+
+
+
+
